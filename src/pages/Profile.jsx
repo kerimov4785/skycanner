de7680c0e9bdfe8f7d2d8bd268c35032efc37b10
kerimov4785 <nihatkerimov4785@gmail.com>
2025-08-06 20:19:35 +0400

    out

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AllContext } from '../context/DataContext'
+import { useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 function Profile() {
+  let { setUser } = useContext(AllContext)
+  let navigate = useNavigate()
+  function exit() {
+    localStorage.removeItem('user')
+    setUser({})
+    navigate('/')
+    toast.success('Logged out of your account')
+  }
   return (
-    <div>Profile</div>
+    <div className='container' >
+      <button onClick={exit} >exit</button>
+    </div>
   )
 }
 
